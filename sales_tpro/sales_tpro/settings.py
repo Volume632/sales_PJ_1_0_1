4d5bf61a49d30db0c22fd773f988ac00956bbb42
Volume632 <zakgleb@gmail.com>
2024-09-05 13:50:01 +0300

    PJ6

--- sales_tpro/sales_tpro/settings.py
+++ sales_tpro/sales_tpro/settings.py
@@ -1,5 +1,9 @@
 
 from pathlib import Path
+import dj_database_url
+import environ
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--^hgzlawj6ov%tp+jl6$50k8p%=wzo3u=mik*y7=3tqnr0wm6h'
+
+
+load_dotenv()  # Загружает переменные из .env файла
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,13 +71,14 @@ WSGI_APPLICATION = 'sales_tpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Инициализация библиотеки django-environ
+env = environ.Env()
+environ.Env.read_env()  # Чтение переменных из .env файла
 
+# Настройка базы данных
+DATABASES = {
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
